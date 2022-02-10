8cd3ac861641f5143cdb2c95c1708d74513e15f7
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-02-10 18:38:37 +0500

    initial

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+//db
+const db = require('./config/keys').mongoURI
 //import routes
 const userRoutes = require('./routes/user')
 //app
 const app = express()
 //mongoose
-mongoose.connect(process.env.DATABASE, (err) => {
-  if (err) throw err
-  console.log('Connected to MongoDB!!!')
-})
+mongoose
+  .connect(db)
+  .then(() => console.log('database is connected'))
+  .catch((err) => console.log(err))
+
 //.then(() => console.log('Database is connected'))
 //routes middleware
 app.use(userRoutes)
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.export = {
+  mongoURI:
+    'mongodb+srv://Syedhasnain:hack82ri.dge@cluster0.bgr1s.mongodb.net/ecommerce?retryWrites=true&w=majority',
+}
