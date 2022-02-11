234a03d3ee3f8249bc18000e71aee0cbd35c44df
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-02-12 02:18:33 +0500

    initial

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 //import routes
 const userRoutes = require('./routes/user')
